a4a48d08a9cdf29a2ce0bfa9b9cc2663085c70ea
Inah0 <hkdychun@qq.com>
2018-12-07 14:06:23 +0800

    Chinese annotation in MultitenancyMySqlApplication 我为什么不用中文呢？因为这是一场试炼

--- src/main/java/com/example/MultitenancyMySqlApplication.java
+++ src/main/java/com/example/MultitenancyMySqlApplication.java
@@ -21,6 +21,16 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
+ * <code>@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })</code>
+ * 如果在配置文件中包含了JPA相关属性，spring boot运行时将会自动配置数据库
+ * 但是在多租户程序中，程序启动时并不会连接上一个默认的数据库
+ * 只有在用户携带租户标识符登录时，才会连接上对应的数据库
+ * 所以通过exclude排除类{@link DataSourceAutoConfiguration}，让spring boot不进行自动数据库配置
+ *
+ *
+ * <code>@SpringBootApplication</code>
+ * 由于关闭了自动进行数据库配置，所以通过此注释进行JPA存储库扫描
+ *
  * @author Sunit Katkar
  * @version 1.0
  * @since 1.0 (April 2018)
